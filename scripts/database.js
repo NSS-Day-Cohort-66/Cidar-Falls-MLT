9f60eefdca0de1285b522cc6b4e293959862f559
Tricia Swift <tricia.swift8@gmail.com>
2023-08-03 15:53:44 -0500

    wip

--- scripts/database.js
+++ scripts/database.js
@@ -1,6 +1,168 @@
 const database = {
-    areas: [],
-    guests: [],
-    parkServices: [],
-    services: [],
-}
+  areas: [
+    {
+      id: 1,
+      name: "Chamfort River",
+    },
+    {
+      id: 2,
+      name: "Lost Wolf Hiking Trail",
+    },
+    {
+      id: 3,
+      name: "Lodge",
+    },
+    {
+      id: 4,
+      name: "Gander River",
+    },
+    {
+      id: 5,
+      name: "Campgrounds",
+    },
+    {
+      id: 6,
+      name: "Pine Bluffs Trails",
+    },
+  ],
+  guests: [],
+  parkServices: [
+    {
+      id: 1,
+      areaId: 1,
+      serviceId: 1,
+    },
+    {
+      id: 1,
+      areaId: 1,
+      serviceId: 2,
+    },
+    {
+      id: 1,
+      areaId: 1,
+      serviceId: 3,
+    },
+    {
+      id: 1,
+      areaId: 2,
+      serviceId: 4,
+    },
+    {
+      id: 1,
+      areaId: 2,
+      serviceId: 5,
+    },
+    {
+      id: 1,
+      areaId: 2,
+      serviceId: 6,
+    },
+    {
+      id: 1,
+      areaId: 3,
+      serviceId: 7,
+    },
+    {
+      id: 1,
+      areaId: 3,
+      serviceId: 8,
+    },
+    {
+      id: 1,
+      areaId: 3,
+      serviceId: 9,
+    },
+    {
+      id: 1,
+      areaId: 3,
+      serviceId: 5,
+    },
+    {
+      id: 1,
+      areaId: 4,
+      serviceId: 1,
+    },
+    {
+      id: 1,
+      areaId: 4,
+      serviceId: 3,
+    },
+    {
+      id: 1,
+      areaId: 1,
+      serviceId: 4,
+    },
+    {
+      id: 1,
+      areaId: 5,
+      serviceId: 9,
+    },
+    {
+      id: 1,
+      areaId: 5,
+      serviceId: 7,
+    },
+    {
+      id: 1,
+      areaId: 5,
+      serviceId: 8,
+    },
+    {
+      id: 1,
+      areaId: 6,
+      serviceId: 4,
+    },
+    {
+      id: 1,
+      areaId: 6,
+      serviceId: 5,
+    },
+    {
+      id: 1,
+      areaId: 6,
+      serviceId: 10,
+    },
+  ],
+  services: [
+    {
+      id: 1,
+      name: "rafting",
+    },
+    {
+      id: 2,
+      name: "canoeing",
+    },
+    {
+      id: 3,
+      name: "fishing",
+    },
+    {
+      id: 4,
+      name: "hiking",
+    },
+    {
+      id: 5,
+      name: "picnicking",
+    },
+    {
+      id: 6,
+      name: "rock climbing",
+    },
+    {
+      id: 7,
+      name: "lodging",
+    },
+    {
+      id: 8,
+      name: "parking",
+    },
+    {
+      id: 9,
+      name: "information",
+    },
+    {
+      id: 10,
+      name: "zip lining",
+    },
+  ],
+};
