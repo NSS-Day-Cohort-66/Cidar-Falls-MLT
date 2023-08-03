e68f22d69585958532ba6dcd42904cdc90519a4c
Luke Hardy <luke.taylor.hardy@gmail.com>
2023-08-03 15:33:03 -0500

    second commit

--- scripts/database.js
+++ scripts/database.js
@@ -4,3 +4,16 @@ const database = {
     parkServices: [],
     services: [],
 }
+
+export const areas = () => {
+    return structuredClone(database.areas)
+}
+export const guests = () => {
+    return structuredClone(database.guests)
+}
+export const parkServices = () => {
+    return structuredClone(database.parkServices)
+}
+export const services = () => {
+    return structuredClone(database.services)
+}
