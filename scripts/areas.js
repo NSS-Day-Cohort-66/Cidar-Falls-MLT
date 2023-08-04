0710dbda4c8a2459c56f4c7a3984e3e4c14f7543
Noor Madkour <noormadkour@gmail.com>
2023-08-04 14:35:51 -0500

    added click event listener to areas to show the number of guests present

--- scripts/areas.js
+++ scripts/areas.js
@@ -1,21 +1,44 @@
-import { getAreas, getServices, getParkServices } from "./database.js";
+import {
+  getAreas,
+  getServices,
+  getParkServices,
+  getGuests,
+} from "./database.js";
 
-const areas = getAreas()
-const parkServices = getParkServices()
-const services = getServices()
+const areas = getAreas();
+const parkServices = getParkServices();
+const services = getServices();
+const guests = getGuests();
 
 export const AreaList = () => {
-  let areaHTML = `<div id="area_card">`
+  let areaHTML = `<div id="area_card">`;
   for (const area of areas) {
-    areaHTML += `<h2>${area.name}</h2>`
+    areaHTML += `<h2 data-id="${area.id}"
+                     data-type="area">${area.name}</h2>`;
     for (const service of parkServices) {
-        for (const specificService of services) {
-            if (service.areaId === area.id && service.serviceId === specificService.id) {
-                areaHTML += `<li>${specificService.name}</li>`
-                }
-            }
+      for (const specificService of services) {
+        if (
+          service.areaId === area.id &&
+          service.serviceId === specificService.id
+        ) {
+          areaHTML += `<li>${specificService.name}</li>`;
         }
-    areaHTML += `</div>`
+      }
     }
-return areaHTML
-}
+    areaHTML += `</div>`;
+  }
+  return areaHTML;
+};
+
+document.addEventListener("click", (areaClick) => {
+  const clickedArea = areaClick.target;
+  if (clickedArea.dataset.type === "area") {
+    let counter = 0;
+    for (const guest of guests) {
+      if (guest.areaId === parseInt(clickedArea.dataset.id)) {
+        counter++;
+      }
+    }
+    window.alert(`There are ${counter} guests in this area`);
+  }
+});
