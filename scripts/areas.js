7fdd480cc1fc2ab9d05342af85331be6d3551b83
Tricia Swift <tricia.swift8@gmail.com>
2023-08-04 14:53:24 -0500

    wip

--- scripts/areas.js
+++ scripts/areas.js
@@ -1,21 +1,25 @@
 import { getAreas, getServices, getParkServices } from "./database.js";
 
-const areas = getAreas()
-const parkServices = getParkServices()
-const services = getServices()
+const areas = getAreas();
+const parkServices = getParkServices();
+const services = getServices();
 
 export const AreaList = () => {
-  let areaHTML = `<div id="area_card">`
+  let areaHTML = `<div class="area_list">`;
   for (const area of areas) {
-    areaHTML += `<h2>${area.name}</h2>`
+    areaHTML += `<div id="area_card"><h2>${area.name}</h2>`;
     for (const service of parkServices) {
-        for (const specificService of services) {
-            if (service.areaId === area.id && service.serviceId === specificService.id) {
-                areaHTML += `<li>${specificService.name}</li>`
-                }
-            }
+      for (const specificService of services) {
+        if (
+          service.areaId === area.id &&
+          service.serviceId === specificService.id
+        ) {
+          areaHTML += `<li>${specificService.name}</li>`;
         }
-    areaHTML += `</div>`
+      }
     }
-return areaHTML
-}
+    areaHTML += `</div>`;
+  }
+  areaHTML += `</div>`;
+  return areaHTML;
+};
